56c8bf9ec06930dda60db7ebd91b12ba64cb7242
Samuel28102005 <labarba.samuel@iisgalvanimi.edu.it>
2024-10-28 11:55:00 +0000

    es 3

--- dbifexist.py
+++ dbifexist.py
@@ -3,9 +3,12 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password ="",
+  password=""
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE mydatabase")
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
